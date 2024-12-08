9bc6b05da0b41f7213fa9e110a00a45f269f4cb3
B L <bl@BdeiMac.local>
2024-12-08 11:17:14 +0800

    feat: 增加词组模式，开始准备关联查找

--- 2.PDF转码.py
+++ 2.PDF转码.py
@@ -16,7 +16,7 @@ import re
 #日志配置文件
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-work_path = "/Users/bl/git/pdf2text/"
+work_path = "/Users/bl/git/pdftToText/"
 
 #下载模块
 def download_pdf(pdf_url, pdf_file_path):
